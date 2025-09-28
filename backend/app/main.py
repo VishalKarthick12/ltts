057be63fcdb3443128779e8d6eb9507953561a0c
vishalkarthickofficial-dot <vishalkarthick.official@gmail.com>
2025-09-28 21:46:13 +0530

    Add backend API URL final

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ cors_origins = os.getenv("CORS_ORIGINS", "http://localhost:3000").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ltts-frontend.onrender.com"],  # hard-coded
+    allow_origins=cors_origins,  # hard-coded
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
